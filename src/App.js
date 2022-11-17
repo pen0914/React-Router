585648a8de89c57b43e743667b9aaa83271550a0
pen0914 <115807679+pen0914@users.noreply.github.com>
2022-11-18 00:19:40 +0900

    2段階のページ遷移 ・render関数の導入 ・propsで、オブジェクト「match」を　　使用

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 import { Page2 } from "./Page2";
 import "./styles.css";
 
@@ -19,9 +21,23 @@ export default function App() {
         <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/Page1">
-          <Page1 />
-        </Route>
+        <Route
+          path="/Page1"
+          render={({ match: { url } }) => (
+            <Switch>
+              <Route exact path={url}>
+                <Page1 />
+              </Route>
+              <Route path={`${url}/detailA`}>
+                <Page1DetailA />
+              </Route>
+              <Route path={`${url}/detailB`}>
+                <Page1DetailB />
+              </Route>
+            </Switch>
+          )}
+        />
+
         <Route path="/Page2">
           <Page2 />
         </Route>
